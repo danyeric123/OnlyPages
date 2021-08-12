371d22f70b81e7d1fa4f7ac4bb8cc1c66626489b
danyeric123 <dnagarpowers@gmail.com>
2021-08-11 20:49:52 -0400

    some components from class added

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,26 +1,64 @@
 import React, { Component } from 'react'
-import { Route } from 'react-router-dom'
+import { Redirect, Route } from 'react-router-dom'
 import NavBar from '../../components/NavBar/NavBar'
 import Signup from '../Signup/Signup'
 import Login from '../Login/Login'
 import Landing from '../Landing/Landing'
+import * as authService from '../../services/authService'
+import ProfileList from '../ProfileList/ProfileList'
+import ProfileDetails from '../ProfileDetails/ProfileDetails'
+import * as profileAPI from '../../services/profileService'
 
 class App extends Component {
-	state = {}
+	state = {
+    user: authService.getUser(),
+    userProfile: null
+  }
+
+	handleLogout = () => {
+    authService.logout();
+    this.setState({ user: null, userProfile: null });
+    this.props.history.push("/");
+  };
+
+  handleSignupOrLogin = async () => {
+    this.setState({ user: await authService.getUser(), userProfile: await profileAPI.getUserProfile()});
+  };
+
+	handleFriend = async (friendId) => {
+    const updatedProfile = await profileAPI.friendAndUnfriend(friendId)
+    this.setState({userProfile: updatedProfile})
+  }
 
 	render() {
+		const { user, userProfile } = this.state
 		return (
 			<>
 				<NavBar user={this.state.user} />
 				<Route exact path='/'>
-          <Landing user={this.state.user} />
+          <Landing user={user} />
         </Route>
 				<Route exact path='/signup'>
-          <Signup history={this.props.history}/>
+          <Signup history={this.props.history} handleSignupOrLogin={this.handleSignupOrLogin}/>
         </Route>
 				<Route exact path='/login'>
-          <Login history={this.props.history}/>
+          <Login handleSignupOrLogin={this.handleSignupOrLogin} history={this.props.history}/>
         </Route>
+				<Route exact path="/users" render={() =>
+					authService.getUser() ?
+					<ProfileList 
+						userProfile={userProfile}
+						// Pass the functions as props to this component
+						handleFriend={this.handleFriend}
+					/> : <Redirect to="/login" />
+				}/>
+				<Route exact path="/profile" render={({ location }) =>
+					authService.getUser() ?
+					<ProfileDetails 
+						userProfile={userProfile}
+						location={location}
+					/> : <Redirect to="/login" />
+				}/>
 			</>
 		)
 	}
