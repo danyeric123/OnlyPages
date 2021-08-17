58ac4e07641382a647e9dcb974e84f3712b47cc0
katiacreator <katia.creator@gmail.com>
2021-08-17 14:26:48 -0400

    added icons to navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,55 +1,77 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import SearchForm from '../SearchForm/SearchForm'
+import React from "react";
+import { Link } from "react-router-dom";
+import SearchForm from "../SearchForm/SearchForm";
+import { FaBook } from "react-icons/fa";
+import { FaUserCircle } from "react-icons/fa";
+import { } from "react-icons/fa";
+import { FaComments } from "react-icons/fa";
 
-const NavBar = ({ user, userProfile, handleLogout, history }) => {
-	return (
-		<>
-			{user ? (
-				<nav>
-					<div>
-						<ul>
-							<li>
-                <Link to="/">LOGO PLACEHOLDER</Link>
+// import { BiSearch } from "react-icons/fa";
+
+const NavBar = ({ user, userProfile, profile, handleLogout, history }) => {
+  console.log(userProfile)
+  return (
+    <>
+      {user ? (
+        <nav>
+          <div>
+            <ul>
+              <li>
+                <Link to="/"><FaBook size={30} style={{ color: "blue" }} />LOGO PLACEHOLDER</Link>
               </li>
-							<SearchForm 
-                history={history}
-              />
-							<li>
+             
+              <SearchForm history={history} />
+              <li>
+                <Link to="/users">
+               Users</Link>
+              </li>
+              <li>
+            {/*   {userProfile.avatar ? (
+                    <img src={userProfile.avatar} alt={userProfile.name} avatar />
+                  ) : (
+                    <FaUserCircle size={70} style={{ color: "blue" }} />
+                  )} */}
+                  <FaUserCircle size={30} style={{ color: "blue" }} />
+                <Link
+                  to={{
+                    pathname: "/profile",
+                    state: { profile: userProfile },
+                  }}
+                >
+                  My Profile
+                </Link>
+              </li>
+              <li>
+                <Link to="/boards">
+                <FaComments size={30} style={{ color: "blue" }} />Message Board</Link>
+              </li>
+              <li>
+                <Link to="" onClick={handleLogout}>
+                  LOG OUT
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      ) : (
+        <nav>
+          <div>
+            <ul>
+              <li>
+                <Link to="/login">Log In</Link>
+              </li>
+              <li>
                 <Link to="/users">Users</Link>
               </li>
-							<li>
-							<Link to={{
-									pathname: '/profile',
-									state: {profile:userProfile}
-								}} >My Profile</Link>
+              <li>
+                <Link to="/signup">Sign Up</Link>
               </li>
-							<li>
-                <Link to="/boards">Message Board</Link>
-              </li>
-							<li><Link to='' onClick={handleLogout}>LOG OUT</Link></li>
-						</ul>
-					</div>
-				</nav>
-			) : (
-				<nav>
-					<div>
-						<ul>
-							<li>
-								<Link to="/login">Log In</Link>
-							</li>
-							<li>
-                <Link to="/users">Users</Link>
-              </li>
-							<li>
-								<Link to="/signup">Sign Up</Link>
-							</li>
-						</ul>
-					</div>
-				</nav>
-			)}
-		</>
-	)
-}
+            </ul>
+          </div>
+        </nav>
+      )}
+    </>
+  );
+};
 
-export default NavBar
+export default NavBar;
