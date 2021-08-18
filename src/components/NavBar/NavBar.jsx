bccd5b9528dcc617ad1d92386798e646d2b31d23
katiacreator <katia.creator@gmail.com>
2021-08-18 17:18:29 -0400

    created button comonent and navbar css file navbar menu is now initially styled

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,70 +3,101 @@ import { Link } from "react-router-dom";
 import SearchForm from "../SearchForm/SearchForm";
 import { FaBook } from "react-icons/fa";
 import { FaUserCircle } from "react-icons/fa";
-import {FaAddressCard } from "react-icons/fa";
+import { FaAddressCard } from "react-icons/fa";
 import { FaComments } from "react-icons/fa";
+import { FaTimes } from "react-icons/fa"; /* for mobil menu */
 import { FaBars } from "react-icons/fa"; /* for mobil menu */
-
+import { Button } from "../Button/Button";
+import "./Navbar.css";
 // import { BiSearch } from "react-icons/fa";
 
 const NavBar = ({ user, userProfile, handleLogout, history }) => {
-  console.log("userProfile:", userProfile)
+  /*   state = {
+    active: false
+  };
+ */
+  /*   handleClick = () => {
+    this.setState({
+      active: !this.state.active
+    })
+  }
+  console.log(state) */
   // console.log("userProfile.avatar:", userProfile.avatar)
   return (
     <>
       {user ? (
-        <nav>
-          <div>
-            <ul>
-              <li>
-                <Link to="/"><FaBook size={30} style={{ color: "blue" }} />OnlyPages</Link>
-              </li>
-             
-              <SearchForm history={history} />
-              <li>
-                <Link to="/users">
-               Users</Link>
-              </li>
-              <li>
-            {/*   {userProfile.avatar ? (
+        <nav className="NavbarItems">
+          <Link to="/">
+            <FaBook
+              size={30}
+              style={{ color: "blue" }}
+              className="navbar-logo"
+            />
+          </Link>
+          <SearchForm history={history} />
+          <div className="menu-icon">
+            <FaTimes
+              className="fas fa-times"
+              size={30}
+              style={{ color: "white" }}
+            />
+            <FaBars
+              className="fas fa-bars"
+              size={30}
+              style={{ color: "white" }}
+            />
+          </div>
+          <ul className="nav-menu active nav-menu">
+            <li className="nav-links">
+              <Link to="/users">Users</Link>
+            </li>
+            <li className="nav-links">
+              {/*   {userProfile.avatar ? (
                     <img src={userProfile.avatar} alt={userProfile.name} avatar />
                   ) : (
                     <FaUserCircle size={70} style={{ color: "blue" }} />
                   )} */}
-                  <FaUserCircle size={30} style={{ color: "blue" }} />
-                <Link
-                  to={{
-                    pathname: `/profiles/${userProfile?._id}`
-                  }}
-                >
-                  My Profile
-                </Link>
-              </li>
-							<li>
-								<Link to='/posts'>All Posts</Link>
-							</li>
-							<li>
-								<Link to='' onClick={handleLogout}>LOG OUT</Link>
-								</li>
-						</ul>
-					</div>
-				</nav>
-			) : (
-				<nav>
-					<div>
-						<ul>
-							<li>
-								<Link to="/login">Log In</Link>
-							</li>
-							<li>
-                <Link to="/users">Users</Link>
-              </li>
-              <li>
-              <FaAddressCard size={30} style={{ color: "blue" }} />
-                <Link to="/signup">Sign Up</Link>
-              </li>
-            </ul>
-          </div>
+              <FaUserCircle size={30} style={{ color: "blue" }} />
+              <Link
+                to={{
+                  pathname: `/profiles/${userProfile?._id}`,
+                }}
+              >
+                My Profile
+              </Link>
+            </li>
+            <li className="nav-links">
+              <FaComments size={30} style={{ color: "blue" }} />
+              <Link to="/posts">All Posts</Link>
+            </li>
+            <li className="nav-links">
+              <Link to="" onClick={handleLogout}>
+                <Button>Logout</Button>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      ) : (
+        <nav className="NavbarItems">
+          <Link to="/">
+            <FaBook
+              size={30}
+              style={{ color: "blue" }}
+              className="navbar-logo"
+            />
+          </Link>
+          <ul className="nav-menu active nav-menu">
+            <li className="nav-links">
+              <Link to="/login">
+                <Button>Login</Button>
+              </Link>
+            </li>
+            <li className="nav-links">
+              <Link to="/signup">
+                <Button>SignUp</Button>
+              </Link>
+            </li>
+          </ul>
         </nav>
       )}
     </>
