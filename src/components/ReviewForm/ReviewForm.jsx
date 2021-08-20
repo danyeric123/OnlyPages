33829c3d3aa1727dd02fe65b1181c0cfb5a73c9f
katiacreator <katia.creator@gmail.com>
2021-08-19 22:43:24 -0400

    finished styling review form

--- src/components/ReviewForm/ReviewForm.jsx
+++ src/components/ReviewForm/ReviewForm.jsx
@@ -34,38 +34,64 @@ const ReviewForm = ({ book, handleAddReview }) => {
 
   return (
     <>
-      <h2>What did you think of this book? Leave a review!</h2>
-      <form
-        ref={formRef}
-        onChange={() => setInvalidForm(!formRef)}
-        onSubmit={handleSubmit}
-      >
-        <textarea
-          type="text"
-          name="content"
-          rows="4"
-          value={content}
-          onChange={({ target }) => setContent(target.value)}
-          required
-          className="appearance-none rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
-        />
-        <div class="flex justify-center items-center">
-          <div class="flex items-center mt-2 mb-4">
-            <label htmlFor="rating">Rating </label>
-            <br />
-            {stars.map((star, idx) =>
-              star ? (
-                <FaStar onClick={() => changeRating(idx)} />
-              ) : (
-                <FaRegStar onClick={() => changeRating(idx)} />
-              )
-            )}
+      <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-md w-full space-y-8">
+          <img
+            src="https://i.imgur.com/ZnXPhEq.jpg"
+            alt="only pages open book logo"
+            className="rounded-full h-20 w-20 mx-auto ring-4 ring-blue hover:opacity-75"
+          />
+          <p className="mt-2 text-center text-2xl font-extrabold text-gray-900">
+            What did you think of this book? 
+          </p>
+          <p className="mt-2 text-center text-xl font-extrabold text-gray-900"> Leave a review and give it a star rating! </p>
+          <form
+            ref={formRef}
+            onChange={() => setInvalidForm(!formRef)}
+            onSubmit={handleSubmit}
+            className="mt-8 space-y-6"
+          >
+            <input type="hidden" name="remember" defaultValue="true" />
+            <div className="rounded-md shadow-sm -space-y-px">
+             
+              <div>
+                <label htmlFor="name" className="sr-only">
+                  Review
+                </label>
+                <textarea
+                  type="text"
+                  name="content"
+                  rows="4"
+                  value={content}
+                  onChange={({ target }) => setContent(target.value)}
+                  required
+                  className="appearance-none rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                />
+              </div>
             </div>
+            <div className="flex justify-center items-center">
+              <div className="flex items-center mt-2 mb-4">
+               
+                <label htmlFor="rating" className="sr-only">Rating</label>
+                {stars.map((star, idx) =>
+                  star ? (
+                    <FaStar onClick={() => changeRating(idx)} />
+                  ) : (
+                    <FaRegStar onClick={() => changeRating(idx)} />
+                  )
+                )}
+              </div>
             </div>
-        <button type="submit" disabled={invalidForm} className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-300 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-          Add Review
-        </button>
-      </form>
+            <button
+              type="submit"
+              disabled={invalidForm}
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-300 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+            >
+              Add Review
+            </button>
+          </form>
+        </div>
+      </div>
     </>
   );
 };
