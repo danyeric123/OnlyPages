14556574568cfa5327fe852d3ed33198567a7b17
danyeric123 <dnagarpowers@gmail.com>
2021-09-05 09:36:25 -0400

    signup form fix

--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -112,7 +112,10 @@ class SignupForm extends Component {
           />
         </div>
         <div>
-          <button className="group relative w-full flex justify-center py-2 border border-transparent text-sm font-medium rounded-md text-white bg-blue-700 hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 my-2">
+          <button 
+            className="group relative w-full flex justify-center py-2 border border-transparent text-sm font-medium rounded-md text-white bg-blue-700 hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 my-2"
+            disabled={!(name && email && password === passwordConf)}  
+          >
             Sign Up
           </button>
           <Link to="/">
